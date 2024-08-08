ed6779d30c3d101b4a6579e0d7138b372a39c66a
Rashidalabri0803 <rashid92759346@gmail.com>
2024-08-08 21:00:09 +0000

    first commit

--- Property_management/settings.py
+++ Property_management/settings.py
@@ -103,12 +103,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Muscat'
 
 USE_I18N = True
-
+USE_L10N =True
 USE_TZ = True
 
 
